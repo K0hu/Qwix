60dd76dcbd33822ae0c312c113df4fbc758c887d
Quentin <k0hu3422@gmail.com>
2026-04-16 13:25:35 +0200

    add search for var

--- src/parser.c
+++ src/parser.c
@@ -22,19 +22,65 @@ char* append(char* str, const char* add_str) {
 }
 
 typedef struct {
+    char name[32];
+    int index;
+} Var;
+
+typedef struct {
     Token *tok;
     char code[1024];
 } Parser;
 
-char load(Token *tok, int offset) {
+char load(Token *tok) {
     
 }
 
-char save(Token *tok, int offset) {
+char save(Token *tok) {
 
 }
 
 Parser parse(Token *tokens, int count) {
+
+    // Search
+    Var vars[1024];
+    int var_count = 0;
+    for (int i = 0; i < count; i++) {
+        switch (tokens[i].type)
+        {
+        case TOKEN_BYTE:
+            strncpy(vars[var_count].name, tokens[i + 1].name, sizeof(vars[var_count].name) - 1);
+            vars[var_count].index = 1;
+            var_count++;
+        
+        case TOKEN_SHORT:
+            strncpy(vars[var_count].name, tokens[i + 1].name, sizeof(vars[var_count].name) - 1);
+            vars[var_count].index = 2;
+            var_count++;
+            break;
+        
+        case TOKEN_INT:
+            strncpy(vars[var_count].name, tokens[i + 1].name, sizeof(vars[var_count].name) - 1);
+            vars[var_count].index = 4;
+            var_count++;
+            break;
+        
+        case TOKEN_FLOAT:
+            strncpy(vars[var_count].name, tokens[i + 1].name, sizeof(vars[var_count].name) - 1);
+            vars[var_count].index = 4;
+            var_count++;
+            break;
+        
+        case TOKEN_DOUBLE:
+            strncpy(vars[var_count].name, tokens[i + 1].name, sizeof(vars[var_count].name) - 1);
+            vars[var_count].index = 8;
+            var_count++;
+            break;
+
+        default:
+            break;
+        }
+    }
+
     int level = 0;
     Parser code;
     code.tok = tokens;
@@ -45,6 +91,9 @@ Parser parse(Token *tokens, int count) {
                 Token right = code.tok[i + 1];
 
                 snprintf(code.code, sizeof(code.code), "mov ");
+                break;
+            default:
+                break;
         }
     }
 }
