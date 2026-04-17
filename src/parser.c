39171f0f31b394874944e90dce102c61df96b49b
Quentin <k0hu3422@gmail.com>
2026-04-17 19:09:56 +0200

    add math for int

--- src/parser.c
+++ src/parser.c
@@ -6,20 +6,39 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdbool.h>
-#include "tokens.h"
+#include <stdint.h>
+#include "lexer.h"
 
 int level = 0;
+const char *regs[] = {
+    "eax", "ecx", "edx",
+    "esi", "edi",
+    "r8d", "r9d", "r10d", "r11d"
+};
+bool reg_used[sizeof(regs)] = {false};
+
+int alloc_reg() {
+    for (int i = 0; i < 4; i++) {
+        if (!reg_used[i]) {
+            reg_used[i] = true;
+            return i;
+        }
+    }
+    fprintf(stderr, "Out of registers\n");
+    exit(1);
+}
+
+void free_reg(int r) {
+    reg_used[r] = false;
+}
 
 // Adds a string to an string
 char* append(char* str, const char* add_str) {
     int len = str ? strlen(str) : 0;
     int add_len = strlen(add_str);
     char* new_str = realloc(str, len + add_len + 1);
-    if (!new_str) {
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
-    strcpy(new_str + len, add_str);  // Append the new string
+    if (!new_str) { fprintf(stderr, "Memory allocation failed\n"); exit(1); }
+    strcpy(new_str + len, add_str);
     return new_str;
 }
 
@@ -38,135 +57,326 @@ typedef struct {
     enum Type type; // 1: byte, 2: short, 4: int, 5: float, 8: double
 } Var;
 
-int find_var_by_name(const char *name, Var *vars, int var_count) {
-    for (int i = 0; i < var_count; i++)
-    {
-        if (strcmp(vars[i].name, name) == 0)
-        {
-            return i; 
-        }
-    }
-    return -1; 
-}
-
 typedef struct {
     Token *tok;
-    char code[1024];
+    char *code;
 } Parser;
 
-enum type load(char *l, size_t size, Token *tok, Var *vars, int var_count) {
+int find_var_by_name(const char *name, Var *vars, int var_count) {
+    for (int i = 0; i < var_count; i++)
+        if (strcmp(vars[i].name, name) == 0) return i;
+    return -1;
+}
+
+enum Type load(char *l, size_t l_size, Token *tok, Var *vars, int var_count) {
     if (tok->type == TOKEN_ID) {
-        // Save variable
         int var_index = find_var_by_name(tok->name, vars, var_count);
-        if (var_index < 0) return;
+        if (var_index < 0) return TYPE_UNDEF;
         switch (vars[var_index].type) {
             case TYPE_BYTE:
-                snprintf(l, size,
-                    "mov al, [rbp-%d]",
-                    vars[var_index].offset);
+                snprintf(l, l_size, "movzx eax, BYTE PTR [rbp-%d]", vars[var_index].offset);
                 return TYPE_BYTE;
-
             case TYPE_SHORT:
-                snprintf(l, size,
-                    "mov ax, [rbp-%d]",
-                    vars[var_index].offset);
+                snprintf(l, l_size, "movzx eax, WORD PTR [rbp-%d]", vars[var_index].offset);
                 return TYPE_SHORT;
-
             case TYPE_INT:
-                snprintf(l, size,
-                    "mov eax, [rbp-%d]",
-                    vars[var_index].offset);
+                snprintf(l, l_size, "mov eax, DWORD PTR [rbp-%d]", vars[var_index].offset);
                 return TYPE_INT;
-
+            case TYPE_FLOAT:
+                snprintf(l, l_size, "movss xmm0, DWORD PTR [rbp-%d]", vars[var_index].offset);
+                return TYPE_FLOAT;
             case TYPE_DOUBLE:
-                snprintf(l, size,
-                    "mov rax, [rbp-%d]",
-                    vars[var_index].offset);
+                snprintf(l, l_size, "movsd xmm0, QWORD PTR [rbp-%d]", vars[var_index].offset);
                 return TYPE_DOUBLE;
-            
-            default:
-                break;
+            default: break;
         }
+    } else if (tok->type == TOKEN_NUMBER) {
+        l[0] = '\0';
+        return TYPE_INT;
     }
     return TYPE_UNDEF;
 }
 
-void save(char *s, size_t size, Token *tok, Var *vars, int var_count) {
-    if (tok->type == TOKEN_ID) {
-        // Save variable
-        int var_index = find_var_by_name(tok->name, vars, var_count);
-        if (var_index < 0) return;
-        switch (vars[var_index].type) {
-            case TYPE_BYTE:
-                snprintf(s, size,
-                    "mov BYTE PTR [rbp-%d], al",
-                    vars[var_index].offset);
-                break;
+static void make_indent(char *buf, size_t buf_size) {
+    int spaces = level * 4;
+    if ((size_t)spaces >= buf_size) spaces = (int)buf_size - 1;
+    memset(buf, ' ', spaces);
+    buf[spaces] = '\0';
+}
 
-            case TYPE_SHORT:
-                snprintf(s, size,
-                    "mov WORD PTR [rbp-%d], ax",
-                    vars[var_index].offset);
-                break;
+enum Expr { NUM, VAR, ADD, SUB, MUL, DIV };
+typedef struct Node {
+    enum Expr type;
+    struct Node *left;
+    struct Node *right;
+    int value;
+    char name[32];
+} Node;
 
-            case TYPE_INT:
-                snprintf(s, size,
-                    "mov DWORD PTR [rbp-%d], eax",
-                    vars[var_index].offset);
-                break;
+Node* parse_expr(Token *tokens, int *pos);
+Node* parse_term(Token *tokens, int *pos);
+Node* parse_factor(Token *tokens, int *pos);
 
-            case TYPE_DOUBLE:
-                snprintf(s, size,
-                    "mov QWORD PTR [rbp-%d], rax",
-                    vars[var_index].offset);
-                break;
-            default:
-                break;
+Node* make_node(enum Expr type, Node *left, Node *right, int value, const char *name) {
+    Node *n = malloc(sizeof(Node));
+    n->type = type;
+    n->left = left;
+    n->right = right;
+    n->value = value;
+    if (name) strncpy(n->name, name, 31);
+    n->name[31] = '\0';
+    return n;
+}
+
+Node* parse_factor(Token *tokens, int *pos) {
+    Token t = tokens[*pos];
+
+    if (t.type == TOKEN_NUMBER) {
+        (*pos)++;
+        return make_node(NUM, NULL, NULL, t.value, NULL);
+    }
+
+    if (t.type == TOKEN_ID) {
+        (*pos)++;
+        return make_node(VAR, NULL, NULL, 0, t.name);
+    }
+
+    if (t.type == TOKEN_LPAREN) {
+        (*pos)++;
+        Node *n = parse_expr(tokens, pos);
+        (*pos)++; // )
+        return n;
+    }
+
+    return NULL;
+}
+
+Node* parse_term(Token *tokens, int *pos) {
+    Node *left = parse_factor(tokens, pos);
+
+    while (tokens[*pos].type == TOKEN_MUL ||
+           tokens[*pos].type == TOKEN_DIV) {
+
+        Token op = tokens[*pos];
+        (*pos)++;
+
+        Node *right = parse_factor(tokens, pos);
+
+        if (op.type == TOKEN_MUL)
+            left = make_node(MUL, left, right, 0, NULL);
+        else
+            left = make_node(DIV, left, right, 0, NULL);
+    }
+
+    return left;
+}
+
+Node* parse_expr(Token *tokens, int *pos) {
+    Node *left = parse_term(tokens, pos);
+
+    while (tokens[*pos].type == TOKEN_MINUS || tokens[*pos].type == TOKEN_PLUS) {
+        Token op = tokens[*pos];
+        (*pos)++;
+
+        Node *right = parse_term(tokens, pos);
+
+        if (op.type == TOKEN_PLUS) {
+            left = make_node(ADD, left, right, 0, NULL);
+        } else {
+            left = make_node(SUB, left, right, 0, NULL);
+        } 
+    }
+
+    return left;
+}
+
+void gen(Node *n, Var *vars, int var_count) {
+
+    if (n->type == NUM) {
+        printf("mov eax, %d\n", n->value);
+        return;
+    }
+
+    if (n->type == VAR) {
+        int idx = find_var_by_name(n->name, vars, var_count);
+        if (idx < 0) {
+            fprintf(stderr, "Unknown variable %s\n", n->name);
+            exit(1);
         }
+        printf("mov eax, DWORD PTR [rbp-%d]\n", vars[idx].offset);
+        return;
+    }
+
+    // LEFT
+    gen(n->left, vars, var_count);
+    printf("push rax\n");
+
+    // RIGHT
+    gen(n->right, vars, var_count);
+
+    printf("mov ebx, eax\n");
+    printf("pop rax\n");
+
+    // NOW:
+    // eax = left
+    // ebx = right
+
+    switch (n->type) {
+        case ADD:
+            printf("add eax, ebx\n");
+            break;
+
+        case SUB:
+            printf("sub eax, ebx\n");
+            break;
+
+        case MUL:
+            printf("imul eax, ebx\n");
+            break;
+
+        case DIV:
+            printf("cdq\nidiv ebx\n");
+            break;
     }
 }
 
-void move(Token *loaded, Token *moved, char *s, size_t size, Var *vars, int var_count) {
-    if (moved->type == TOKEN_ID) {
-        char l[32];
-        size_t size = sizeof(l);
-        int spaces_per_level = 4;
-        char indent[level * spaces_per_level + 1];
-        
-        memset(indent, ' ', level * spaces_per_level);
-        
-        indent[level * spaces_per_level] = '\0';
-        enum type t = load(&l, size, &moved, vars, var_count);
-        if (loaded->type == TOKEN_ID) {
+void printNode(Node *n, int indent) {
+    for (int i = 0; i < indent; i++) printf("  ");
 
-        } else {
-            
-            int index = find_var_by_name();
+    switch (n->type) {
+        case NUM: printf("%d\n", n->value); break;
+        case VAR: printf("%s\n", n->name); break;
+        case ADD: printf("+\n"); break;
+        case SUB: printf("-\n"); break;
+        case MUL: printf("*\n"); break;
+        case DIV: printf("/\n"); break;
+    }
+
+    if (n->left) printNode(n->left, indent + 1);
+    if (n->right) printNode(n->right, indent + 1);
+}
+
+// move: generates assembly for: loaded = moved
+// loaded: destination (variable or register name)
+// moved:  source (variable or number literal)
+void move(Token *loaded, Token *moved, char *s, size_t size, Var *vars, int var_count) {
+    char l[256];
+    size_t l_size = sizeof(l);
+    char indent[128];
+    make_indent(indent, sizeof(indent));
+ 
+    if (moved->type == TOKEN_ID) {
+        // Source is a variable  load it first
+        enum Type t = load(l, l_size, moved, vars, var_count);
+ 
+        int dst_index = find_var_by_name(loaded->name, vars, var_count);
+ 
+        if (dst_index >= 0 && loaded->type == TOKEN_ID) {
+            // var = var
             switch (t) {
                 case TYPE_BYTE:
-                    snprintf(s, size,
-                        "%s\n%smov WORD PTR [rbp-%d], al",
-                        l, indent, );
-                    break;
-
+                    snprintf(s, size, "%s\n%smov BYTE PTR [rbp-%d], al",
+                        l, indent, vars[dst_index].offset); break;
                 case TYPE_SHORT:
-                    snprintf(s, size,
-                        "mov WORD PTR [rbp-%d], ax",
-                        vars[var_index].offset);
-                    break;
-
+                    snprintf(s, size, "%s\n%smov WORD PTR [rbp-%d], ax",
+                        l, indent, vars[dst_index].offset); break;
                 case TYPE_INT:
-                    snprintf(s, size,
-                        "mov DWORD PTR [rbp-%d], eax",
-                        vars[var_index].offset);
-                    break;
-
+                    snprintf(s, size, "%s\n%smov DWORD PTR [rbp-%d], eax",
+                        l, indent, vars[dst_index].offset); break;
+                case TYPE_FLOAT:
+                    snprintf(s, size, "%s\n%smovss DWORD PTR [rbp-%d], xmm0",
+                        l, indent, vars[dst_index].offset); break;
                 case TYPE_DOUBLE:
+                    snprintf(s, size, "%s\n%smovsd QWORD PTR [rbp-%d], xmm0",
+                        l, indent, vars[dst_index].offset); break;
+                default: break;
+            }
+        } else {
+            // reg = var  (e.g. mov rax, QWORD PTR [rbp-8])
+            int src_index = find_var_by_name(moved->name, vars, var_count);
+            if (src_index < 0) return;
+            switch (t) {
+                case TYPE_BYTE:
+                    snprintf(s, size, "%smovzx %s, BYTE PTR [rbp-%d]",
+                        indent, loaded->name, vars[src_index].offset); break;
+                case TYPE_SHORT:
+                    snprintf(s, size, "%smovzx %s, WORD PTR [rbp-%d]",
+                        indent, loaded->name, vars[src_index].offset); break;
+                case TYPE_INT:
+                    snprintf(s, size, "%smov %s, DWORD PTR [rbp-%d]",
+                        indent, loaded->name, vars[src_index].offset); break;
+                case TYPE_FLOAT:
+                    snprintf(s, size, "%smovss %s, DWORD PTR [rbp-%d]",
+                        indent, loaded->name, vars[src_index].offset); break;
+                case TYPE_DOUBLE:
+                    snprintf(s, size, "%smovsd %s, QWORD PTR [rbp-%d]",
+                        indent, loaded->name, vars[src_index].offset); break;
+                default: break;
+            }
+        }
+ 
+    } else if (moved->type == TOKEN_NUMBER) {
+        // Source is a number literal
+        int dst_index = find_var_by_name(loaded->name, vars, var_count);
+ 
+        if (dst_index >= 0 && loaded->type == TOKEN_ID) {
+            // var = immediate
+            enum Type t = vars[dst_index].type;
+            switch (t) {
+                case TYPE_BYTE:
+                    snprintf(s, size, "%smov BYTE PTR [rbp-%d], %d ; def",
+                        indent, vars[dst_index].offset, (int)moved->value); break;
+                case TYPE_SHORT:
+                    snprintf(s, size, "%smov WORD PTR [rbp-%d], %d ; def",
+                        indent, vars[dst_index].offset, (int)moved->value); break;
+                case TYPE_INT:
+                    snprintf(s, size, "%smov DWORD PTR [rbp-%d], eax ; def",
+                        indent, vars[dst_index].offset, (int)moved->value); break;
+                case TYPE_FLOAT: {
+                    // float immediate  eax  xmm0  memory
+                    float f = (float)moved->value;
+                    uint32_t bits;
+                    memcpy(&bits, &f, sizeof(bits));
                     snprintf(s, size,
-                        "mov QWORD PTR [rbp-%d], rax",
-                        vars[var_index].offset);
+                        "%smov eax, 0x%08X\n%smovd xmm0, eax\n%smovss DWORD PTR [rbp-%d], xmm0 ; def",
+                        indent, bits, indent, indent, vars[dst_index].offset);
                     break;
+                }
+                case TYPE_DOUBLE: {
+                    // double immediate  rax  xmm0  memory
+                    double d = moved->value;
+                    uint64_t bits;
+                    memcpy(&bits, &d, sizeof(bits));
+                    snprintf(s, size,
+                        "%smov rax, 0x%016lX\n%smovq xmm0, rax\n%smovsd QWORD PTR [rbp-%d], xmm0 ; def",
+                        indent, bits, indent, indent, vars[dst_index].offset);
+                    break;
+                }
+                default: break;
+            }
+        } else {
+            // reg = immediate
+            if (dst_index < 0) return;
+            switch (vars[dst_index].type) {
+                case TYPE_FLOAT: {
+                    float f = (float)moved->value;
+                    uint32_t bits;
+                    memcpy(&bits, &f, sizeof(bits));
+                    snprintf(s, size, "%smov eax, 0x%08X\n%smovd %s, eax",
+                        indent, bits, indent, loaded->name);
+                    break;
+                }
+                case TYPE_DOUBLE: {
+                    double d = moved->value;
+                    uint64_t bits;
+                    memcpy(&bits, &d, sizeof(bits));
+                    snprintf(s, size, "%smov rax, 0x%016lX\n%smovq %s, rax",
+                        indent, bits, indent, loaded->name);
+                    break;
+                }
                 default:
+                    snprintf(s, size, "%smov %s, %d",
+                        indent, loaded->name, (int)moved->value);
                     break;
             }
         }
@@ -228,8 +438,68 @@ Parser parse(Token *tokens, int count) {
     }
 
     Parser code;
+    code.code = malloc(1); 
+    code.code[0] = '\0'; 
     code.tok = tokens;
+    bool default_case = false;
+    Node *n;
     for (int i = 0; i < count; i++) {
-        
+        char line[512];
+        switch (tokens[i].type) {
+            case TOKEN_ARROWL:
+                move(&tokens[i - 1], &tokens[i + 1], line, (size_t)512, vars, var_count);
+                code.code = append(code.code, line);
+                default_case = false;
+                break;
+            
+            case TOKEN_ARROWR:
+                default_case = false;
+                if (tokens[i + 1].type == TOKEN_BYTE || tokens[i + 1].type == TOKEN_SHORT || tokens[i + 1].type == TOKEN_INT ||
+                    tokens[i + 1].type == TOKEN_FLOAT || tokens[i + 1].type == TOKEN_DOUBLE) {
+                    // var declaration with initialization, e.g. "10 -> int x"
+                    move(&tokens[i + 2], &tokens[i - 1], line, (size_t)512, vars, var_count);
+
+                } else {
+                    move(&tokens[i + 1], &tokens[i - 1], line, (size_t)512, vars, var_count);
+                }
+                code.code = append(code.code, line);
+                break;
+            
+            case TOKEN_NEWLINE:
+                code.code = append(code.code, "\n");
+                default_case = false;
+                break;
+            
+            case TOKEN_NUMBER:
+                n = parse_expr(tokens, &i);
+                gen(n, vars, var_count);   
+
+                default_case = false;
+                break;
+
+            default:
+                default_case = true;
+                break;
+        }
+        if (!default_case) {
+            code.code = append(code.code, "\n");
+        }
     }
+
+    return code;
+}
+
+// test
+int main() {
+    const char *code = 
+        "int x <- 5 + 4 * 6";
+
+    int count = 0;
+    Token *tokens = tokenize(code, &count);
+    Parser parsed = parse(tokens, count);
+
+    printf(parsed.code);
+
+    free(tokens);
+    return 0;
 }
